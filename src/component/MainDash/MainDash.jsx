77efe7d875d7549a5dedb1fdae17cd04ba57b8de
arashesmailian <arashesmailian@gmail.com>
2022-05-24 21:51:43 +0430

    table added

--- src/component/MainDash/MainDash.jsx
+++ src/component/MainDash/MainDash.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Cards from '../Cards/Cards'
+import './MainDash.css'
+import Table from '../Table/Table'
 
 const MainDash = () => {
   return (
     <div className="MainDash">
         <h1>Dashboard</h1>
         <Cards />
+        <Table/>
     </div>
   )
 }
